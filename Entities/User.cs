aad23d83929ff93590e7a4a9929cebab7e3472e5
Hammond <Hieutruonghoang01@gmail.com>
2025-11-06 11:09:44 +0700

    refactor(domain): remove old User entity from root Entities folder
    - Remove deprecated User.cs from root Entities folder
    - Entity now exists in Domain/Entities/User.cs following DDD structure

--- Entities/User.cs
+++
@@ -1,9 +0,0 @@
-namespace MyBatis.TestApp.Entities
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; } = "";
-        public string Email { get; set; } = "";
-    }
-}
